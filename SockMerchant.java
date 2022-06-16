cfdb64008b5524ec16cb0501ed3a93facc13ad29
Dharmendra Bhanushali <35648838+DharmendraBhanushali@users.noreply.github.com>
2022-06-16 19:51:18 +0530

    Create SockMerchant.java
    There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
    
    Example:
    n=7
    ar= [1,2,1,2,1,3,2]
    
    There is one pair of color 1  and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2 .
    
    Function Description
    
    Complete the sockMerchant function in the editor below.
    
    sockMerchant has the following parameter(s):
    
    int n: the number of socks in the pile
    int ar[n]: the colors of each sock
    Returns
    
    int: the number of pairs

---
+++ SockMerchant.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+   int result = 0;
+   
+   Map<Integer,Long> map = ar.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+   
+   for(Long value : map.values()){
+       
+       if(value%2==0){
+           result = (int) (result+(value/2));
+       }
+       else{
+           int tempValue = (int)(value%2);
+           result = (int) (result+((value-tempValue)/2));
+       }
+       
+   }
+   
+   return result;
+
+    }
+
+}
+
+public class SockMerchant {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
