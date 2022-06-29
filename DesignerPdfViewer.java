d7e2d304e8315c9d1734cc1d9ea56a5e537b4f97
Dharmendra Bhanushali <35648838+DharmendraBhanushali@users.noreply.github.com>
2022-06-29 18:54:05 +0530

    Create DesignerPdfViewer.java
    When a contiguous block of text is selected in a PDF viewer, the selection is highlighted with a blue rectangle. In this PDF viewer, each word is highlighted independently.

---
+++ DesignerPdfViewer.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<Integer> h, String word) {
+  
+    int result = 0;
+    Map<Character, Integer> map = new HashMap<>();
+    for(int i=0;i<h.size();i++){
+        char c = (char)(97+i);
+        map.put(Character.valueOf(c), h.get(i));
+    }
+    
+    char [] w = word.toCharArray();
+    int max = 0;
+    for(char s : w ){
+        max = Math.max(max, map.get(Character.valueOf(s)));
+    }
+    result = max*w.length;
+    return result;
+    }
+
+}
+
+public class DesignerPdfViewer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String word = bufferedReader.readLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
